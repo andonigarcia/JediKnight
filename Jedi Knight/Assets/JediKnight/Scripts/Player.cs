6b9f0e5be1a8e500bd116e220b542995884e3a01
Andoni Garcia <andoni@cs.uchicago.edu>
2016-02-17 13:32:04 -0600

    Rudimentary networking with lightsaber movement

---
+++ Jedi Knight/Assets/JediKnight/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 40f;
+
+	private NetworkView nView;
+
+	void OnNetworkInstantiate(NetworkMessageInfo info) {
+		nView = GetComponent<NetworkView>();
+		if (nView.isMine)
+			Debug.Log("New object instanted by me");
+		else
+			Debug.Log("New object instantiated by " + info.sender);
+	}
+
+	void Update()
+	{
+		if (nView.isMine)
+		{
+			InputMovement ();
+		}
+	}
+
+	void InputMovement()
+	{
+		Vector3 movement = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.W))
+			movement = Vector3.back;
+		else if (Input.GetKey (KeyCode.S))
+			movement = Vector3.forward;
+		else if (Input.GetKey (KeyCode.D))
+			movement = Vector3.right;
+		else if (Input.GetKey (KeyCode.A))
+			movement = Vector3.left;
+
+		foreach (Transform child in gameObject.GetComponentsInChildren<Transform>() )
+		{
+			Debug.Log(child.tag);
+			if (child.CompareTag ("Saber"))
+			{
+				child.Rotate(movement * speed * Time.deltaTime);
+			}
+		}
+	}
+}
