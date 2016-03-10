d9351024a9825f3fc2b9c37ab2124668181c3fc3
Zoe Naidoo <uznaidoo@gmail.com>
2016-03-10 16:40:58 -0600

    Zoes bullets and shiet

---
+++ Jedi Knight/Assets/Downloaded Assets/Ethan Enemies/HealthBar.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+
+var healthBarSlider : UnityEngine.UI.Slider;
+
+function Start () {
+
+}
+
+function Update () {
+
+	
+}
+
+
+function OnTriggerEnter (other : Collider) {
+		Destroy(other.gameObject);
+		healthBarSlider.value -=.02f;
+}
