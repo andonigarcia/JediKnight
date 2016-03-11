a70b796dc01e66c61b59030f38a80620f9f6b9eb
Andoni Garcia <andoni@cs.uchicago.edu>
2016-03-11 11:10:29 -0600

    Ethan enemies take 2

---
+++ Jedi Knight/Assets/Downloaded Assets/Ethan Enemies/DisplayScore.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var score : UnityEngine.UI.Text;
+var count : int;
+
+public class DisplayScore extends MonoBehaviour{
+
+function Start () {
+	count = 0;
+}
+
+function Update () {
+	score.text = "Score " + count;
+}
+
+public function enemyKilled(){
+	count++;
+}
+
+}
