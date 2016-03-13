268df0349a004600275b557525b597dda762f664
Zoe Naidoo <uznaidoo@gmail.com>
2016-03-13 16:05:51 -0500

    enemy locations chosen

--- Jedi Knight/Assets/Downloaded Assets/Ethan Enemies/TeleportEnemies.cs
+++ Jedi Knight/Assets/Downloaded Assets/Ethan Enemies/TeleportEnemies.cs
@@ -21,13 +21,27 @@ using UnityEngine.UI;
 public class TeleportEnemies : MonoBehaviour {
 	public Transform ethan;
 	public Transform prefab;
+	private int lastPos = 0;
 
   private Vector3 startingPosition;
+
+	Vector3[] positionArray = new Vector3[7];
+
 
   void Start() {
     startingPosition = transform.localPosition;
 	transform.LookAt(ethan);
     SetGazedAt(false);
+
+	positionArray[0] = new Vector3(0.46f,2.17f,1.23f);            
+	positionArray[1] = new Vector3(-0.51f,1.99f,1.19f);
+	positionArray[2] = new Vector3(-0.79f,2.13f,0.47f);
+	positionArray[3] = new Vector3(1.19f,2.46f,0.38f);
+	positionArray[4] = new Vector3(0.44f,1.03f,0.78f);
+	positionArray[5] = new Vector3(0.23f,3.47f,0.62f);
+	positionArray[6] = new Vector3(1.11f,3.3f,0.73f);
+
+
   }
 
   void LateUpdate() {
@@ -65,14 +79,25 @@ public class TeleportEnemies : MonoBehaviour {
 	}
 
   public void TeleportRandomly() {
-	Debug.Log ("I am teleporting randomly");
 
+	for (int j = 0; j < 50000000; j++) {
+		}
+			
 	Vector3 direction = Random.onUnitSphere;
     //direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
     //float distance = 2 * Random.value + 1.5f;
-	float howFarAwayIsEthan = Vector3.Distance(ethan.position, this.transform.position);
-	//have to change teleport position to one of the random ones around ethan
-	transform.localPosition = direction * howFarAwayIsEthan;
+	//float howFarAwayIsEthan = Vector3.Distance(ethan.position, this.transform.position);
+	//transform.localPosition = direction * howFarAwayIsEthan;
+	int i =  (int)Mathf.Floor(Random.Range(0.0f,6.0f));
+
+		while (i == lastPos) {
+			i = (int)Mathf.Floor(Random.Range(0.0f,6.0f));
+		}
+	
+	transform.localPosition =positionArray[i];
+	lastPos = i;
+	
+
 	transform.LookAt(ethan);
 	
   }
